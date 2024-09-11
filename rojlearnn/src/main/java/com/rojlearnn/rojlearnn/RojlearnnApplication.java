4bde500c56357c4d2a8c749439f88d6440aaf36b
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-09-11 14:27:59 +0000

    setup

---
+++ rojlearnn/src/main/java/com/rojlearnn/rojlearnn/RojlearnnApplication.java
@@ -0,0 +1,13 @@
+package com.rojlearnn.rojlearnn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojlearnnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RojlearnnApplication.class, args);
+	}
+
+}
