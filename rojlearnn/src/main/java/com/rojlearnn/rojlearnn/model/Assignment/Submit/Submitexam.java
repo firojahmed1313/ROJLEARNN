3dae6804baf8112af71b3539f5fc1f81d18e57ce
firojahmed1313 <firozahmed3052000@gmail.com>
2024-09-28 23:18:12 +0530

    Submit

--- rojlearnn/src/main/java/com/rojlearnn/rojlearnn/model/Assignment/Submit/Submitexam.java
+++ rojlearnn/src/main/java/com/rojlearnn/rojlearnn/model/Assignment/Submit/Submitexam.java
@@ -2,7 +2,25 @@ package com.rojlearnn.rojlearnn.model.Assignment.Submit;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "submitexam")
 public class Submitexam {
+    private ObjectId _id;
+    private String ans;
 
+    public Submitexam(){}
+    public Submitexam(ObjectId id, String ans) {
+        this._id = id;
+        this.ans = ans;
+    }
+    public String getId(){
+        return _id.toHexString();
+    }
+    public void setId(String id){
+        this._id = new ObjectId(id);
+    }
+    public String getAns(){
+        return ans;
+    }
+    public void setAns(String ans){
+        this.ans = ans;
+    }
 }
--- rojlearnn/src/main/java/com/rojlearnn/rojlearnn/model/Assignment/Submit/Submittask.java
+++ rojlearnn/src/main/java/com/rojlearnn/rojlearnn/model/Assignment/Submit/Submittask.java
@@ -2,7 +2,28 @@ package com.rojlearnn.rojlearnn.model.Assignment.Submit;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "submittask")
 public class Submittask {
+    private ObjectId _id;
+    private String answer;
+
+    // Getters and Setters
+    public String getAnswer() {
+        return answer;
+    }
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+    public String get_id() {
+        return _id.toString();
+    }
+    public void set_id(String _id) {
+        this._id = new ObjectId(_id);
+    }
+
+    public Submittask(){}
+    public Submittask(ObjectId id,String answer){
+        this._id = id;
+        this.answer = answer;
+    }
 
 }
