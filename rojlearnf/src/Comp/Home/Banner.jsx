35756a78ad640b37f9ecf699f53fe7d3e355b25e
firojahmed1313 <firozahmed3052000@gmail.com>
2024-10-01 18:53:51 +0530

    Home Page

--- rojlearnf/src/Comp/Home/Banner.jsx
+++ rojlearnf/src/Comp/Home/Banner.jsx
@@ -6,20 +6,20 @@ const Banner = () => {
 
 
         < section
-            className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat border rounded-3xl"
+            className="relative bg-[url(https://images.unsplash.com/photo-1610484826917-0f101a7bf7f4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)] bg-cover bg-center bg-no-repeat border rounded-3xl"
         >
             <div
                 className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
             ></div>
 
             <div
-                className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 border border-red-500"
+                className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8 "
             >
                 <div className="max-w-xl text-left ltr:sm:text-left rtl:sm:text-right">
                     <h1 className="text-3xl font-extrabold text-white sm:text-5xl">
                         Let us find your
 
-                        <strong className="block font-extrabold text-rose-500"> Forever Home. </strong>
+                        <strong className="block font-extrabold text-rose-500"> Forever Gole. </strong>
                     </h1>
 
                     <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
