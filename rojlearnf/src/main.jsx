fbc66a1661649901596d58c97b816a2eaab552f8
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-10-06 13:39:35 +0000

    store

--- rojlearnf/src/main.jsx
+++ rojlearnf/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './Redux/Store.js'
+import { store } from './Redux/Store'
 createRoot(document.getElementById('root')).render(
   
     <StrictMode>
--- rojlearnn/src/main/java/com/rojlearnn/rojlearnn/RojlearnnApplication.java
+++ rojlearnn/src/main/java/com/rojlearnn/rojlearnn/RojlearnnApplication.java
@@ -2,11 +2,9 @@ package com.rojlearnn.rojlearnn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-//@EnableMongoRepositories(basePackages ={"UserRepo.class", "CourseRepo.class"})
 @SpringBootApplication
 @CrossOrigin(origins = "https://8080-firojahmed131-rojlearnn-yx462bjcym9.ws-us116.gitpod.io")
 public class RojlearnnApplication {
