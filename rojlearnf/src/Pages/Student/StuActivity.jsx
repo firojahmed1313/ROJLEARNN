d762f49b6464fb3db546f61a9498988bcc95df89
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-10-18 13:26:28 +0000

    select dropdown

--- rojlearnf/src/Pages/Student/StuActivity.jsx
+++ rojlearnf/src/Pages/Student/StuActivity.jsx
@@ -8,6 +8,12 @@ import StudentFeedback from '../../Comp/Student/lcrf/StudentFeedback'
 import StudentTracking from '../../Comp/Student/lcrf/StudentTracking'
 
 const StuActivity = () => {
+  const [tab, setTab] = React.useState("Like");
+
+  const onOptionChangeHandler = (event) => {
+    setTab(event.target.value);
+    console.log(event.target.value);
+  }
   return (
     <>
       <div className='flex flex-row '>
@@ -17,11 +23,64 @@ const StuActivity = () => {
         <div className='w-5/6 absolute  right-0 '>
           <Topnav />
           <div>
-            <StudentLike />
-            <StudentComment />
-            <StudentRating />
-            <StudentFeedback  />
-            <StudentTracking />
+            
+
+            <div className='mb-4'>
+              <div className="sm:hidden">
+                <label htmlFor="Tab" className="sr-only">Tab</label>
+
+                <select id="Tab" className="w-full h-10 text-center text-white border-gray-200 bg-gray-800" onChange={onOptionChangeHandler}>
+                  <option>Like</option>
+                  <option>Comment</option>
+                  <option>Rating</option>
+                  <option>Feedback</option>
+                  <option>Tracking</option>
+                  
+                </select>
+              </div>
+
+              <div className=" hidden sm:block w-full ml-3">
+                <nav className="flex gap-6" aria-label="Tabs">
+                  <button
+                    onClick={() => setTab("Like")}
+                    className={`${tab === "Like" ? " bg-sky-100  text-sky-600" : "text-gray-500 border-transparent"} rounded-lg shrink-0  p-2 text-sm font-medium hover:bg-gray-50 hover:text-gray-700`}
+                  >
+                    Like
+                  </button>
+
+                  <button
+                    onClick={() => setTab("Comment")}
+                    className={`${tab === "Comment" ? " bg-sky-100  text-sky-600" : "text-gray-500 border-transparent"} rounded-lg shrink-0  p-2 text-sm font-medium hover:bg-gray-50 hover:text-gray-700`}
+                  >
+                    Comment
+                  </button>
+                  <button
+                    onClick={() => setTab("Rating")}
+                    className={`${tab === "Rating" ? " bg-sky-100  text-sky-600" : "text-gray-500 border-transparent"} rounded-lg shrink-0  p-2 text-sm font-medium hover:bg-gray-50 hover:text-gray-700`}
+                  >
+                    Rating
+                  </button>
+                  <button
+                    onClick={() => setTab("Feedback")}
+                    className={`${tab === "Feedback" ? " bg-sky-100  text-sky-600" : "text-gray-500 border-transparent"} rounded-lg shrink-0  p-2 text-sm font-medium hover:bg-gray-50 hover:text-gray-700`}
+                  >
+                    Feedback
+                  </button>
+                  <button
+                    onClick={() => setTab("Tracking")}
+                    className={`${tab === "Tracking" ? " bg-sky-100  text-sky-600" : "text-gray-500 border-transparent"} rounded-lg shrink-0  p-2 text-sm font-medium hover:bg-gray-50 hover:text-gray-700`}
+                  >
+                    Tracking
+                  </button>
+                </nav>
+              </div>
+            </div>
+            {tab === "Like" && <StudentLike />}
+            {tab === "Comment" && <StudentComment />}
+            {tab === "Rating" && <StudentRating />}
+            {tab === "Feedback" && <StudentFeedback />}
+            {tab === "Tracking" && <StudentTracking />}
+
           </div>
         </div>
       </div>
--- rojlearnf/src/Pages/Student/StuExam.jsx
+++ rojlearnf/src/Pages/Student/StuExam.jsx
@@ -4,7 +4,13 @@ import Topnav from '../../Comp/Navber/Topnav'
 import StudentsAssignment from '../../Comp/Student/StudentsAssignment'
 import StudentsTask from '../../Comp/Student/StudentsTask'
 import StudentsExam from '../../Comp/Student/StudentsExam'
+import { Link } from 'react-router-dom'
 const StuExam = () => {
+  const [tab, setTab] = React.useState("Exam");
+  const onOptionChangeHandler = (event) => {
+    setTab(event.target.value);
+    console.log(event.target.value);
+  };
   return (
     <>
       <div className='flex flex-row '>
@@ -13,10 +19,54 @@ const StuExam = () => {
         </div>
         <div className='w-5/6 absolute  right-0 '>
           <Topnav />
-          <div>
-            <StudentsAssignment />
-            <StudentsTask />
-            <StudentsExam />
+          <div className="w-full mb-4">
+
+
+            <div>
+              <div className="sm:hidden">
+                <label htmlFor="Tab" className="sr-only">Tab</label>
+
+                <select id="Tab" className="w-full h-10 text-center text-white border-gray-200 bg-gray-800" onChange={onOptionChangeHandler}>
+                  <option >Exam</option>
+                  <option >Assignment</option>
+                  <option > Task</option>
+                </select>
+              </div>
+
+              <div className=" hidden sm:block ml-3">
+                <div className="border-b border-gray-200">
+                  <nav className="-mb-px flex gap-6">
+                    <button
+                      onClick={() => setTab("Exam")}
+                      className={`${tab === "Exam" ? "shrink-0 rounded-t-lg border-b-white border-gray-300 text-sky-600" : "text-gray-500 border-transparent"} shrink-0 border p-3 text-sm font-medium text-gray-500 hover:text-gray-700`}
+                    >
+                      Exam
+                    </button>
+
+                    <button
+                      onClick={() => setTab("Assignment")}
+                      className={`${tab === "Assignment" ? "shrink-0 rounded-t-lg border-b-white border-gray-300 text-sky-600" : "text-gray-500 border-transparent"} shrink-0 border p-3 text-sm font-medium text-gray-500 hover:text-gray-700`}
+                    >
+                      Assignment
+                    </button>
+
+                    
+
+                    <button
+                      onClick={() => setTab("Task")}
+                      className={`${tab === "Task" ? "shrink-0 rounded-t-lg border-b-white border-gray-300 text-sky-600" : "text-gray-500 border-transparent"} shrink-0 border p-3 text-sm font-medium text-gray-500 hover:text-gray-700`}
+                    >
+                      Task
+                    </button>
+                  </nav>
+                </div>
+              </div>
+            </div>
+
+            {tab === "Exam" && <StudentsExam />}
+            {tab === "Assignment" && <StudentsAssignment />}
+            {tab === "Task" && <StudentsTask />}
+            
           </div>
         </div>
       </div>
