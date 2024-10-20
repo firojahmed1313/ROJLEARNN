7736f2eb06944687284a06b0c354880282087c38
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-10-20 07:15:15 +0000

    task details forntend

--- rojlearnf/src/Comp/Student/Details/StudentTaskDetails.jsx
+++ rojlearnf/src/Comp/Student/Details/StudentTaskDetails.jsx
@@ -9,26 +9,94 @@ import { useLocation } from 'react-router-dom'
 
 const StudentTaskDetails = () => {
   const location = useLocation();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const id = location.pathname.substring(13);
-    console.log(id);
-    const token = Cookies.get('ROJLEARN');
-    console.log(token);
-    const taskDetails = useSelector((state) => state.getTaskById.taskDetails);
-    console.log(taskDetails);
-    useEffect(() => {
-        if (!token) {
-            navigate("/login");
-        }
-        else {
-          if (taskDetails == null) {
-            dispatch(getTaskDetails(id));
-          }
-        }
-    }, [id])
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const id = location.pathname.substring(13);
+  console.log(id);
+  const token = Cookies.get('ROJLEARN');
+  console.log(token);
+  const taskDetails = useSelector((state) => state.getTaskById.taskDetails);
+  console.log(taskDetails);
+  useEffect(() => {
+    if (!token) {
+      navigate("/login");
+    }
+    else {
+      if (taskDetails == null) {
+        dispatch(getTaskDetails(id));
+      }
+    }
+  }, [id])
   return (
-    <div>StudentTaskDetails</div>
+    <>
+      <div class="items-center flex flex-wrap">
+        <div class="hidden lg:block w-full md:w-4/12 ml-auto mr-auto px-4">
+          <img alt="..." class="max-w-full rounded-lg shadow-lg" src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" />
+        </div>
+        <div class="w-[96%] lg:w-5/12 ml-auto mr-auto px-4 mx-auto">
+          <div class="md:pr-12">
+            <div class="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300 mt-8">
+              <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+                <g fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
+                  <path d="M22 6H9C7.34315 6 6 7.34315 6 9V31C6 32.6569 7.34315 34 9 34H39C40.6569 34 42 32.6569 42 31V22" />
+                  <path d="M24 34V42" />
+                  <path d="M14 42L34 42" />
+                  <path fill="#2f88ff" d="M33.3 6C31.4775 6 30 7.43473 30 9.20455C30 12.4091 33.9 15.3223 36 16C38.1 15.3223 42 12.4091 42 9.20455C42 7.43473 40.5225 6 38.7 6C37.5839 6 36.5972 6.53804 36 7.3616C35.4028 6.53804 34.4161 6 33.3 6Z" />
+                </g>
+              </svg>
+            </div>
+            <h3 class="text-3xl font-semibold">{taskDetails?.title}</h3>
+            <p class="mt-4 text-lg leading-relaxed text-blueGray-500">
+              {taskDetails?.descriptions}
+            </p>
+            <h5 class="text-lg mt-4 font-bold">{taskDetails?.taskquestions}</h5>
+            <ul class="list-none mt-6">
+              <li class="py-2">
+                <div class="flex items-center">
+                  <div>
+                    <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"><svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+                      <g fill="none" stroke="#9f7aea" stroke-linejoin="round" stroke-width="4">
+                        <path d="M24 44c11.046 0 20-8.954 20-20S35.046 4 24 4S4 12.954 4 24s8.954 20 20 20Z" />
+                        <path stroke-linecap="round" d="M24.008 12v12.01l8.479 8.48" />
+                      </g>
+                    </svg></span>
+                  </div>
+                  <div>
+                    <h4 class="text-blueGray-500">
+                      {taskDetails?.totalduration} h
+                    </h4>
+                  </div>
+                </div>
+              </li>
+              <li class="py-2">
+                <div class="flex items-center">
+                  <div>
+                    <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 48 48">
+                      <path fill="none" stroke="#9f7aea" stroke-linecap="round" stroke-linejoin="round" d="M24 2.5A21.5 21.5 0 1 0 45.5 24A21.51 21.51 0 0 0 24 2.5m0 11.18l3.59 6.54l7.18 1.51l-5 5.51l.82 7.46L24 31.56l-6.68 3.1l.87-7.45l-5-5.54l7.2-1.46z" />
+                    </svg></span>
+                  </div>
+                  <div>
+                    <h4 class="text-blueGray-500">{taskDetails?.totalmarks}</h4>
+                  </div>
+                </div>
+              </li>
+              <li class="py-2">
+                <div class="flex items-center">
+                  <div>
+                    <span class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                      <path fill="black" d="M18 15H6l-4 4V3a1 1 0 0 1 1-1h15a1 1 0 0 1 1 1v11a1 1 0 0 1-1 1m5-6v14l-4-4H8a1 1 0 0 1-1-1v-1h14V8h1a1 1 0 0 1 1 1M8.19 4c-.87 0-1.57.2-2.11.59c-.52.41-.78.98-.77 1.77l.01.03h1.93c.01-.3.1-.53.28-.69a1 1 0 0 1 .66-.23c.31 0 .57.1.75.28c.18.19.26.45.26.75c0 .32-.07.59-.23.82c-.14.23-.35.43-.61.59c-.51.34-.86.64-1.05.91C7.11 9.08 7 9.5 7 10h2c0-.31.04-.56.13-.74s.26-.36.51-.52c.45-.24.82-.53 1.11-.93s.44-.81.44-1.31c0-.76-.27-1.37-.81-1.82C9.85 4.23 9.12 4 8.19 4M7 11v2h2v-2zm6 2h2v-2h-2zm0-9v6h2V4z" />
+                    </svg></span>
+                  </div>
+                  <div>
+                    <h4 class="text-blueGray-500">{taskDetails?.totalquestions}</h4>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
