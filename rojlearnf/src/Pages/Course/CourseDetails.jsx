527959fae6f7d1758ed38bf9898485977f7c1de1
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-10-20 11:36:32 +0000

    home course css

--- rojlearnf/src/Pages/Course/CourseDetails.jsx
+++ rojlearnf/src/Pages/Course/CourseDetails.jsx
@@ -36,7 +36,7 @@ const CourseDetails = () => {
         <>
             <Nav />
             {
-                (course.data) ?
+                (course?.data) ?
                     <section className="text-gray-600 body-font overflow-hidden">
                         <div className="container px-5 py-16 mx-auto">
                             <div className="lg:w-4/5 mx-auto flex flex-wrap">
@@ -48,8 +48,8 @@ const CourseDetails = () => {
                                     Save 10%
                                 </span>
                                 <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                                    <h2 className="text-sm title-font text-gray-500 tracking-widest">{course.data.category}</h2>
-                                    <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{course.data.title}</h1>
+                                    <h2 className="text-sm title-font text-gray-500 tracking-widest">{course.data?.category.toUpperCase()}</h2>
+                                    <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{course.data.title} <sup class="bg-blue-100  text-blue-800 text-2xl font-semibold me-2 px-2.5 py-0.5 relative  rounded dark:bg-blue-200 dark:text-blue-800 ms-2">{course.data.duration_hours} Hours</sup></h1>
                                     <div className="flex mb-4">
                                         <span className="flex items-center">
                                             <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
@@ -87,7 +87,7 @@ const CourseDetails = () => {
                                             </a>
                                         </span>
                                     </div>
-                                    <p className="leading-relaxed">{courseData.description}</p>
+                                    <p className="leading-relaxed">{course.data.description}</p>
                                     {/* <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                                         <div className="flex">
                                             <span className="mr-3">Color</span>
@@ -112,13 +112,14 @@ const CourseDetails = () => {
                                             </div>
                                         </div>
                                     </div> */}
-                                    <div className="flex">
+                                    <div className="flex mt-4">
                                         <span className="title-font font-medium text-2xl text-gray-900">${course.data.price}</span>
-                                        <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
+                                        <button className="flex ml-auto  text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">{course.data.is_published ? "Buy Now" : "Comming Soon"}</button>
                                         <button className="rounded-full w-10 h-10 bg-indigo-500 hover:bg-indigo-600 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                                             {/* <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                                                 <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
                                             </svg> */}
+
                                             <svg className="h-6 w-6 text-white" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                                                 <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path>
                                             </svg>
