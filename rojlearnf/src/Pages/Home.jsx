4a99c6ba17e0c4b03236ea73a0a75904df4fc8b1
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-10-20 10:30:31 +0000

    Home

--- rojlearnf/src/Pages/Home.jsx
+++ rojlearnf/src/Pages/Home.jsx
@@ -17,11 +17,11 @@ const Home = () => {
   const loading = useSelector((state) => state.getCourse.isLoading);
   const error = useSelector((state) => state.getCourse.error);
   console.log(course);
-  useEffect(() => {
-    if (course != null) {
-      setCourseData(course.data);
-    }
-  }, [course])
+  // useEffect(() => {
+  //   if (course != null) {
+  //     setCourseData(course.data);
+  //   }
+  // }, [course])
 
 
   useEffect(() => {
@@ -46,25 +46,25 @@ const Home = () => {
           (loading) ? <CartColloder /> :
             <div className="flex -mx-3">
               <div className="w-full max-w-full mb-8 md:w-1/2 mx-4 lg:w-1/3 flex flex-col rounded-md border">
-                <img src="https://images.unsplash.com/photo-1711349172514-3f11275bfbb0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Card img" className="object-cover object-center w-full h-48 rounded-md" />
+                <img src={course?.data[0]?.thumbnail_url} alt="Card img" className="object-cover object-center w-full h-48 rounded-md" />
                 <div className="flex flex-grow">
                   <div className="triangle"></div>
                   <div className="flex flex-col justify-between px-4 py-6 bg-white  text">
                     <div>
                       <Link href="#"
-                        className="inline-block mb-4 text-xs font-bold capitalize border-b-2 border-blue-600 hover:text-blue-600">Reliable
-                        Schemas</Link>
+                        className="inline-block mb-4 text-xs font-bold capitalize border-b-2 border-blue-600 hover:text-blue-600">
+                        {course?.data[0]?.category}
+                        </Link>
                       <Link href="#"
                         className="block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
-                        What Zombies Can Teach You About Food
+                        {course?.data[0]?.title}
                       </Link>
                       <p className="mb-4">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla delectus corporis commodi
-                        aperiam, amet cupiditate?
+                        {course?.data[0]?.description}
                       </p>
                     </div>
                     <div>
-                      <Link href="#"
+                      <Link to={`/courses/${course?.data[0]?._id}`}
                         className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
                         More </Link>
                     </div>
@@ -72,25 +72,23 @@ const Home = () => {
                 </div>
               </div>
               <div className="w-full hidden md:flex  max-w-full mb-8 md:w-1/2 mx-4 lg:w-1/3 flex-col rounded-md border">
-                <img src="https://images.unsplash.com/photo-1711349172514-3f11275bfbb0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Card img" className="object-cover object-center w-full h-48 rounded-md" />
+                <img src={course?.data[1]?.thumbnail_url} alt="Card img" className="object-cover object-center w-full h-48 rounded-md" />
                 <div className="flex flex-grow">
                   <div className="triangle"></div>
                   <div className="flex flex-col justify-between px-4 py-6 bg-white  text">
                     <div>
                       <Link href="#"
-                        className="inline-block mb-4 text-xs font-bold capitalize border-b-2 border-blue-600 hover:text-blue-600">Reliable
-                        Schemas</Link>
+                        className="inline-block mb-4 text-xs font-bold capitalize border-b-2 border-blue-600 hover:text-blue-600">{course?.data[1]?.category}</Link>
                       <Link href="#"
                         className="block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
-                        What Zombies Can Teach You About Food
+                        {course?.data[1]?.title}
                       </Link>
                       <p className="mb-4">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla delectus corporis commodi
-                        aperiam, amet cupiditate?
+                        {course?.data[1]?.description}
                       </p>
                     </div>
                     <div>
-                      <Link href="#"
+                      <Link to={`/courses/${course?.data[1]?._id}`}
                         className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
                         More </Link>
                     </div>
@@ -98,25 +96,24 @@ const Home = () => {
                 </div>
               </div>
               <div className="w-full hidden lg:flex max-w-full mb-8 sm:w-1/2 mx-4 lg:w-1/3  flex-col rounded-md border">
-                <img src="https://images.unsplash.com/photo-1711349172514-3f11275bfbb0?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Card img" className="object-cover object-center w-full h-48 rounded-md" />
+                <img src={course?.data[2]?.thumbnail_url} alt="Card img" className="object-cover object-center w-full h-48 rounded-md" />
                 <div className="flex flex-grow">
                   <div className="triangle"></div>
                   <div className="flex flex-col justify-between px-4 py-6 bg-white  text">
                     <div>
                       <Link href="#"
-                        className="inline-block mb-4 text-xs font-bold capitalize border-b-2 border-blue-600 hover:text-blue-600">Reliable
-                        Schemas</Link>
+                        className="inline-block mb-4 text-xs font-bold capitalize border-b-2 border-blue-600 hover:text-blue-600">
+                        {course?.data[2]?.category}</Link>
                       <Link href="#"
                         className="block mb-4 text-2xl font-black leading-tight hover:underline hover:text-blue-600">
-                        What Zombies Can Teach You About Food
+                        {course?.data[2]?.title}
                       </Link>
                       <p className="mb-4">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla delectus corporis commodi
-                        aperiam, amet cupiditate?
+                        {course?.data[2]?.description}
                       </p>
                     </div>
                     <div>
-                      <Link href="#"
+                      <Link to={`/courses/${course?.data[2]?._id}`}
                         className="inline-block pb-1 mt-2 text-base font-black text-blue-600 uppercase border-b border-transparent hover:border-blue-600">Read
                         More </Link>
                     </div>
