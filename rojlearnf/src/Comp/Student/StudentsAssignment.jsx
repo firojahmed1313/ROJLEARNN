df7b0e4a00b6b9b94897bdc84c7b2ab262eca407
firojahmed1313 <firozahmed3052000@gmail.com>
2024-10-23 11:04:36 +0530

    AET Students

--- rojlearnf/src/Comp/Student/StudentsAssignment.jsx
+++ rojlearnf/src/Comp/Student/StudentsAssignment.jsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import { getCourseofStudent } from '@/Redux/Features/Course/getCourseofStudentSlice';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom'
+import AETST from '../utlits/AETST';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const data = [
   {
@@ -25,49 +34,45 @@ const data = [
 ]
 const StudentsAssignment = () => {
   const navigate = useNavigate();
+    const courses = useSelector((state) => state.getCourseofStudent.course);
+    console.log("courses", courses);
+    const user = useSelector((state) => state.getUser.user);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        setTimeout(() => {
+            if (!courses && user != null) {
+              dispatch(getCourseofStudent(user._id));
+            }
+        }, 2000);
+    }, []);
   return (
     <>
       <div>
         <p className=" text-center text-blue-700 my-4 text-3xl font-bold">All Assignment</p>
       </div>
 
-      {
-        (data.length != 0)
-          ?
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 w-[96%] mx-auto">
-            {
-              data?.map((item) => {
-                return (
-                  <div
-                    onClick={() => navigate(`/studentAssignment/${item.id}`)}
-                    class=" bg-white shadow-[0px_0px_15px_rgba(0,0,0,0.09)] p-9 space-y-3 relative overflow-hidden cursor-pointer"
-                  >
-                    <div class="w-24 h-24 bg-violet-500 rounded-full absolute -right-5 -top-7">
-                      <p class="absolute bottom-6 left-7 text-white text-2xl">{item.id}</p>
-                    </div>
-                    <div class="fill-violet-500 w-20">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
-                        <g fill="none" stroke="#9f7aea" stroke-linecap="round" stroke-linejoin="round" stroke-width="4">
-                          <path d="M21 6H9a3 3 0 0 0-3 3v22a3 3 0 0 0 3 3h30a3 3 0 0 0 3-3V21M24 34v8" />
-                          <path d="m32 6l-4 4l4 4m6-8l4 4l-4 4M14 42h20" />
-                        </g>
-                      </svg>
-                    </div>
-                    <h1 class="font-bold text-xl">{item.title}</h1>
-                    <p class="text-sm text-zinc-500 leading-6">
-                      {item.desc}
-                    </p>
-                  </div>
-                )
-              })
-            }
-
-
-
-
+      
+        {courses?.length != 0 ? (
+          <div className="grid grid-cols-1 w-[98%] mx-auto ">
+              <Accordion type="single" collapsible className="w-[98%] mx-auto  ">
+                  {courses?.map((course) => {
+                      return (
+                          
+                          <AccordionItem value={`item-${course._id}`} className="my-3 transition-all duration-200 bg-white border border-gray-200 shadow-lg cursor-pointer hover:bg-gray-50">
+                              <AccordionTrigger className=" px-4 flex text-xl font-bold text-black">{course.title}</AccordionTrigger>
+                              <AccordionContent className="p-4 text-sm text-zinc-500 leading-6">
+                                  <AETST data={data} />
+                              </AccordionContent>
+                          </AccordionItem>
+  
+                      );
+                  })}
+              </Accordion>
           </div>
-          : <h4>No Assignment</h4>
-      }
+      ) : (
+          <h4>No Task</h4>
+      )}
+      
 
 
     </>
