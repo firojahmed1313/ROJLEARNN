fdb1c096f06c134fa2fe10a82f404d58b91d94b4
MD FIROJ AHMED <firozahmed3052000@gmail.com>
2024-11-21 05:32:24 +0000

    filter frontend

--- rojlearnf/src/Comp/Home/Courseall.jsx
+++ rojlearnf/src/Comp/Home/Courseall.jsx
@@ -61,7 +61,7 @@ const Courseall = () => {
                     </header>
 
                     <div className="mt-8 block lg:hidden">
-                        <button
+                        {/* <button
                             className="flex cursor-pointer items-center gap-2 border-b border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
                         >
                             <span className="text-sm font-medium"> Filters & Sorting </span>
@@ -76,20 +76,229 @@ const Courseall = () => {
                             >
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
                             </svg>
-                        </button>
+                        </button> */}
+                        <div className="grid grid-cols-2 md:grid-cols-6 gap-8 ">
+                            <div className="mr-4 " >
+                                <select id="SortBy" className="mt-1 rounded border-gray-300 text-sm font-medium ">
+                                    <option>Sort By</option>
+                                    <option value="Title, DESC">Duration, DESC</option>
+                                    <option value="Title, ASC">Duration, ASC</option>
+                                    <option value="Price, DESC">Price, DESC</option>
+                                    <option value="Price, ASC">Price, ASC</option>
+                                    <option value="Price, DESC">Date, DESC</option>
+                                    <option value="Price, ASC">Date, ASC</option>
+                                </select>
+                            </div>
+                            <div className="relative md:ml-4">
+                                <details className="group [&_summary::-webkit-details-marker]:hidden w-fit">
+                                    <summary
+                                        className="flex cursor-pointer items-center gap-2 border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
+                                    >
+                                        <span className="text-sm font-medium"> Availability </span>
+
+                                        <span className="transition group-open:-rotate-180">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth="1.5"
+                                                stroke="currentColor"
+                                                className="size-4"
+                                            >
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </span>
+                                    </summary>
+
+                                    <div className="z-50 group-open:absolute group-open:right-0  group-open:top-auto group-open:mt-2 shadow-xl shadow-slate-600">
+                                        <div className="w-48 rounded border border-gray-200 bg-white">
+                                            <header className="flex items-center justify-between p-4">
+                                                <span className="text-sm text-gray-700"> 0 Selected </span>
+
+                                                <button type="button" className="text-sm text-gray-900 underline underline-offset-4">
+                                                    Reset
+                                                </button>
+                                            </header>
+
+                                            <ul className="space-y-1 border-t border-gray-200 p-4">
+                                                <li>
+                                                    <label htmlFor="FilterInStock" className="inline-flex items-center gap-2">
+                                                        <input type="checkbox" id="FilterInStock" className="size-5 rounded border-gray-300" />
+
+                                                        <span className="text-sm font-medium text-gray-700"> In Stock (5+) </span>
+                                                    </label>
+                                                </li>
+
+                                                <li>
+                                                    <label htmlFor="FilterPreOrder" className="inline-flex items-center gap-2">
+                                                        <input type="checkbox" id="FilterPreOrder" className="size-5 rounded border-gray-300" />
+
+                                                        <span className="text-sm font-medium text-gray-700"> Pre Order (3+) </span>
+                                                    </label>
+                                                </li>
+
+                                                <li>
+                                                    <label htmlFor="FilterOutOfStock" className="inline-flex items-center gap-2">
+                                                        <input
+                                                            type="checkbox"
+                                                            id="FilterOutOfStock"
+                                                            className="size-5 rounded border-gray-300"
+                                                        />
+
+                                                        <span className="text-sm font-medium text-gray-700"> Out of Stock (10+) </span>
+                                                    </label>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </details>
+                            </div>
+                            <div className="relative">
+                                <details className="group [&_summary::-webkit-details-marker]:hidden w-fit">
+                                    <summary
+                                        className="flex cursor-pointer items-center gap-2 border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
+                                    >
+                                        <span className="text-sm font-medium"> Price </span>
+
+                                        <span className="transition group-open:-rotate-180">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth="1.5"
+                                                stroke="currentColor"
+                                                className="size-4"
+                                            >
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </span>
+                                    </summary>
+
+                                    <div className="z-50 group-open:absolute group-open:right-0 group-open:top-auto group-open:mt-2 shadow-xl shadow-slate-600">
+                                        <div className="w-48 rounded border border-gray-200 bg-white">
+                                            
+                                            <div className="border-t border-gray-200 p-4">
+                                                <div className="flex justify-between gap-4">
+                                                    <label htmlFor="FilterPriceFrom" className="flex items-center gap-2">
+                                                        <span className="text-sm text-gray-600">$</span>
+
+                                                        <input
+                                                            type="number"
+                                                            id="FilterPriceFrom"
+                                                            placeholder="From"
+                                                            className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                                        />
+                                                    </label>
+
+                                                    <label htmlFor="FilterPriceTo" className="flex items-center gap-2">
+                                                        <span className="text-sm text-gray-600">$</span>
+
+                                                        <input
+                                                            type="number"
+                                                            id="FilterPriceTo"
+                                                            placeholder="To"
+                                                            className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                                        />
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <header className="flex items-center justify-evenly p-4">
+
+                                                <button type="button" className="text-sm text-gray-900 underline underline-offset-4">
+                                                    Go
+                                                </button>
+                                                <button type="button" className="text-sm text-gray-900 underline underline-offset-4">
+                                                    Reset
+                                                </button>
+                                            </header>
+                                        </div>
+                                    </div>
+                                </details>
+                            </div>
+                            <div className="relative">
+                                <details className="group [&_summary::-webkit-details-marker]:hidden w-fit">
+                                    <summary
+                                        className="flex cursor-pointer items-center gap-2 border-gray-400 pb-1 text-gray-900 transition hover:border-gray-600"
+                                    >
+                                        <span className="text-sm font-medium"> Category </span>
+
+                                        <span className="transition group-open:-rotate-180">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth="1.5"
+                                                stroke="currentColor"
+                                                className="size-4"
+                                            >
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </span>
+                                    </summary>
+
+                                    <div className="z-50 group-open:absolute group-open:right-0 group-open:top-auto group-open:mt-2 shadow-xl shadow-slate-600">
+                                        <div className="w-48 rounded border border-gray-200 bg-white">
+                                            <header className="flex items-center justify-between p-4">
+                                                <span className="text-sm text-gray-700"> 0 Selected </span>
+
+                                                <button type="button" className="text-sm text-gray-900 underline underline-offset-4">
+                                                    Reset
+                                                </button>
+                                            </header>
+
+                                            <ul className="space-y-1 border-t border-gray-200 p-4">
+                                                <li>
+                                                    <label htmlFor="FilterInStock" className="inline-flex items-center gap-2">
+                                                        <input type="checkbox" id="FilterInStock" className="size-5 rounded border-gray-300" />
+
+                                                        <span className="text-sm font-medium text-gray-700"> In Stock (5+) </span>
+                                                    </label>
+                                                </li>
+
+                                                <li>
+                                                    <label htmlFor="FilterPreOrder" className="inline-flex items-center gap-2">
+                                                        <input type="checkbox" id="FilterPreOrder" className="size-5 rounded border-gray-300" />
+
+                                                        <span className="text-sm font-medium text-gray-700"> Pre Order (3+) </span>
+                                                    </label>
+                                                </li>
+
+                                                <li>
+                                                    <label htmlFor="FilterOutOfStock" className="inline-flex items-center gap-2">
+                                                        <input
+                                                            type="checkbox"
+                                                            id="FilterOutOfStock"
+                                                            className="size-5 rounded border-gray-300"
+                                                        />
+
+                                                        <span className="text-sm font-medium text-gray-700"> Out of Stock (10+) </span>
+                                                    </label>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </details>
+                            </div>
+                        </div>
+
+                        <button type="button" className=" mt-6 rounded-md bg-gray-200 px-4 py-3 text-md font-medium text-gray-900 border-b-2 ">Clear Filters</button>
                     </div>
 
                     <div className="mt-4 lg:mt-8 lg:grid lg:grid-cols-4 lg:items-start lg:gap-8">
                         <div className="hidden space-y-4 lg:block">
+                        <button type="button" className=" mt-6 rounded-md bg-gray-200 px-4 py-3 text-md font-medium text-gray-900 border-b-2 ">Clear Filters</button>
+
                             <div>
                                 <label htmlFor="SortBy" className="block text-xs font-medium text-gray-700"> Sort By </label>
 
                                 <select id="SortBy" className="mt-1 rounded border-gray-300 text-sm">
                                     <option>Sort By</option>
-                                    <option value="Title, DESC">Title, DESC</option>
-                                    <option value="Title, ASC">Title, ASC</option>
+                                    <option value="Title, DESC">Duration, DESC</option>
+                                    <option value="Title, ASC">Duration, ASC</option>
                                     <option value="Price, DESC">Price, DESC</option>
                                     <option value="Price, ASC">Price, ASC</option>
+                                    <option value="Price, DESC">Date, DESC</option>
+                                    <option value="Price, ASC">Date, ASC</option>
                                 </select>
                             </div>
 
@@ -241,7 +450,7 @@ const Courseall = () => {
                                         <summary
                                             className="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition"
                                         >
-                                            <span className="text-sm font-medium"> Colors </span>
+                                            <span className="text-sm font-medium"> Category </span>
 
                                             <span className="transition group-open:-rotate-180">
                                                 <svg
@@ -395,7 +604,9 @@ const Courseall = () => {
 
                         </div>
 
+
                     </div>
+
                 </div>
                 <div className="md:hidden w-full mx-auto left-0 fixed bottom-0">
                     <ButtomNavHome tab="Course" />
